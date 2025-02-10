b3a78ed5a27002437f21a661019498265133a961
v0idp <voidp@protonmail.com>
2025-02-10 20:32:59 +0100

    finished custom helm patch loading

--- include/SynthControllerPatch.hpp
+++ include/SynthControllerPatch.hpp
@@ -1,13 +1,18 @@
 #pragma once
+#include "System/zzzz__Exception_def.hpp"
 #include "VROSC/SynthController.hpp"
+#include "VROSC/SynthDataController.hpp"
 #include "AudioHelm/HelmPatchFormat.hpp"
-#include "paper2_scotland2/shared/logger.hpp"
+#include "VROSC/PatchSettings.hpp"
+#include "UnityEngine/JsonUtility.hpp"
+#include "main.hpp"
+#include <exception>
 
 MAKE_HOOK_MATCH(SynthController_SynthsDataLoaded, &VROSC::SynthController::SynthsDataLoaded, void, VROSC::SynthController* self, VROSC::InstrumentDataController*  dataController) {
     SynthController_SynthsDataLoaded(self, dataController);
 
     // set up directory to load helm patches from
-    std::string directory = "/sdcard/Android/data/com.Really.Virtuoso/files/mods/import/patches";
+    std::string directory = "/sdcard/ModData/com.Really.Virtuoso/import/patches";
 
     // load all helm patches from the directory into a vector
     std::vector<std::filesystem::path> files;
@@ -22,9 +27,23 @@ MAKE_HOOK_MATCH(SynthController_SynthsDataLoaded, &VROSC::SynthController::Synth
 
         // load all helm patches into the synth controller
         for (const auto& file : files) {
-            // TODO: load the helm patch here
+            try
+            {
+                std::string content = readfile(file.string());
+                AudioHelm::HelmPatchFormat* patch = UnityEngine::JsonUtility::FromJson<AudioHelm::HelmPatchFormat*>(content);
+                patch->patch_name = file.stem().string();
+                self->get_SynthDataController()->_defaultPatches->Add(VROSC::PatchSettings::New_ctor(patch));
+                self->get_SynthDataController()->AddPatch(patch);
+                PaperLogger.info("Added custom patch: {}", file.stem().string());
+            }
+            catch (std::exception* e)
+            {
+                PaperLogger.error("Failed to add patch: {}", file.stem().string());
+                PaperLogger.error("{}", e->what());
+            }
+            
         }
     } else {
-        PaperLogger.info("No custom patches found.");
+        PaperLogger.info("Couldn't find any custom helm patches!");
     }
 }
